c4ada821d1b066fdc88d8eabb907fdb0c4cad1a3
DanDan <DanDan@DanDan.noreply.github.com>
2017-02-13 20:09:11 +0800

    初始化项目
    初始化项目

---
+++ GPDesignMode/GPDesignMode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPDesignMode
+//
+//  Created by dandan on 17/2/13.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GPDesignMode/GPDesignMode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPDesignMode
+//
+//  Created by dandan on 17/2/13.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
