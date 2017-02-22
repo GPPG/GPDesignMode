15bacc150f3c7ea3662e0bb0be9f88fa52a7718d
DanDan <821403986@qq.com>
2017-02-22 11:59:42 +0800

    005

---
+++ GPDesignMode/GPDesignMode/GPFiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPFiveViewController.h
+//  GPDesignMode
+//
+//  Created by dandan on 17/2/22.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import "GPBaseViewController.h"
+
+@interface GPFiveViewController : GPBaseViewController
+
+@end
---
+++ GPDesignMode/GPDesignMode/GPFiveViewController.m
@@ -0,0 +1,37 @@
+//
+//  GPFiveViewController.m
+//  GPDesignMode
+//
+//  Created by dandan on 17/2/22.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import "GPFiveViewController.h"
+
+@interface GPFiveViewController ()
+
+@end
+
+@implementation GPFiveViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
