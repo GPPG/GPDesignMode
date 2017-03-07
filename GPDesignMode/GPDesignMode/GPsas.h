412b29df6e775a99dc729ef7d285408199f80035
DanDan <82140396@qq.com>
2017-03-07 21:25:19 +0800

    ddd

---
+++ GPDesignMode/GPDesignMode/GPsas.h
@@ -0,0 +1,13 @@
+//
+//  GPsas.h
+//  GPDesignMode
+//
+//  Created by 郭鹏 on 2017/3/7.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPsas : NSObject
+
+@end
---
+++ GPDesignMode/GPDesignMode/GPsas.m
@@ -0,0 +1,13 @@
+//
+//  GPsas.m
+//  GPDesignMode
+//
+//  Created by 郭鹏 on 2017/3/7.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import "GPsas.h"
+
+@implementation GPsas
+
+@end
