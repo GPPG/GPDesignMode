b11055738a53b15d0e842ec3f41f86200db9e12f
DanDan <821403986@qq.com>
2017-03-15 20:34:38 +0800

    16--

---
+++ GPDesignMode/GPDesignMode/GPSixteenViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPSixteenViewController.h
+//  GPDesignMode
+//
+//  Created by 郭鹏 on 2017/3/12.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import "GPBaseViewController.h"
+
+@interface GPSixteenViewController : GPBaseViewController
+
+@end
