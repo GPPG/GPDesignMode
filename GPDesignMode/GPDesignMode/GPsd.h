c5fe7e477095656970960bdad67e80204cdca213
DanDan <821403986@qq.com>
2017-03-23 23:22:29 +0800

    可怕

---
+++ GPDesignMode/GPDesignMode/GPsd.h
@@ -0,0 +1,13 @@
+//
+//  GPsd.h
+//  GPDesignMode
+//
+//  Created by 郭鹏 on 2017/3/23.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import "GPExpression.h"
+
+@interface GPsd : GPExpression
+
+@end
---
+++ GPDesignMode/GPDesignMode/GPsd.m
@@ -0,0 +1,13 @@
+//
+//  GPsd.m
+//  GPDesignMode
+//
+//  Created by 郭鹏 on 2017/3/23.
+//  Copyright © 2017年 dandan. All rights reserved.
+//
+
+#import "GPsd.h"
+
+@implementation GPsd
+
+@end
